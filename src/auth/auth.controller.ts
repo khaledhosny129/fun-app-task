47adad00408c9bc38d3c2dfb1c94f1353772f1f0
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-11-14 00:15:32 +0200

    auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -9,6 +14,8 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiBadRequestResponse({ description: 'No user found with this email' })
+  @ApiUnauthorizedResponse({ description: 'Incorrect password' })
   @ApiBody({ type: LoginDto })
   async login(@Body() loginDto: LoginDto): Promise<any> {
     const user = await this.authService.validateUserCreds(
