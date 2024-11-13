cd978cf14f1008a6805fd0384d0ec216776ccb19
khaledhosny129 <86989101+khaledhosny129@users.noreply.github.com>
2024-11-14 00:08:30 +0200

    auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -9,7 +8,7 @@ import { UsersModule } from '../users/users.module';
 import { jwtConfig } from '../../config/jwt.config';
 
 @Module({
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   imports: [UsersModule, PassportModule, JwtModule.registerAsync(jwtConfig)],
 })
